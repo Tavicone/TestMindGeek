7e2945d145f37101e3ecc8e7d95dfdf8d9463887
Octavian Lelescu <octavian.lelescu@arnia.ro>
2019-10-21 22:11:42 +0300

    add unit test for Image store proccess

---
+++ mindgeek/tests/Unit/ImageStoreTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Libraries\ImageStore;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+class ImageStoreTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testAlbumUpload()
+    {
+        Storage::fake('public');
+
+        // Assert one or more files were stored...
+        $validImageUrl = 'https://mgtechtest.blob.core.windows.net/images/unscaled/2012/11/29/Parental-Guidance-VPA.jpg';
+        $hasedImage = md5($validImageUrl);
+        $pos = strrpos($validImageUrl, '.');
+        $extension = substr($validImageUrl, $pos + 1);
+
+        ImageStore::getImageFromUrl($validImageUrl);
+
+        Storage::disk('public')->assertExists($hasedImage.'.'.$extension);
+
+        // Assert one invalid image is not stored...
+        $invalidImageUrl = 'https://mgtechtest.blob.core.windows.net/images/unscaled/2013/07/15/LPA-Parental-guidance.jpg';
+        $hasedImage = md5($invalidImageUrl);
+        $pos = strrpos($invalidImageUrl, '.');
+        $extension = substr($invalidImageUrl, $pos + 1);
+
+        ImageStore::getImageFromUrl($invalidImageUrl);
+
+        Storage::disk('public')->assertMissing($hasedImage.'.'.$extension);
+    }
+}
